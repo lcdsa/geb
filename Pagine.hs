b1e985ac53c626abb607c03394fabf11b3a2a080
Claudio Pannacci <pannacciclaudio@gmail.com>
2021-01-01 19:15:40 +0100

    inizio pagina 37

--- Pagine.hs
+++ Pagine.hs
@@ -7,7 +7,12 @@ import Test.QuickCheck
 
 pag :: Int -> IO ()
 pag 36 = do
-      putStrLn "Ecco alcune stringhe del sistema MIU:"
-      sample (arbitrary :: Gen MIUString)
-      putStrLn ""
+   putStrLn "Alcune stringhe del sistema MIU:"
+   sample (arbitrary :: Gen MIU.String)
+   putStrLn ""
+pag 37 = do
+   putStrLn "Stringhe in nostro possesso:"
+   putStrLn ""
+   putStrLn $ unlines $ map show theorems
+
 pag _ = putStrLn "Niente di interessante..."
