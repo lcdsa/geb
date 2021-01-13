7eea0bc8b73345e7a068708e6afd3cdd1525f75f
Arianna Masciolini <uzkamascio@gmail.com>
2021-01-13 10:15:48 +0100

    fixed rest in royal theme

--- RoyalTheme.hs
+++ RoyalTheme.hs
@@ -7,7 +7,7 @@ royalTheme = foldl (:+:) (rest 0) [
     -- bar by bar
     c 5 hn, ef 5 hn, 
     g 5 hn, af 5 hn, 
-    b 4 hn, rest 1, g 5 qn, 
+    b 4 hn, rest (1/4), g 5 qn, 
     fs 5 hn, f 5 hn,
     e 5 hn, ef 5 dhn, 
     d 5 qn, df 5 qn, c 5 qn, 
