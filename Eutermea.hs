1e66a11035fa65cb5f31e9daf84b5967b9dd4b40
Arianna Masciolini <uzkamascio@gmail.com>
2021-01-14 14:50:03 +0100

    added fromList for monodic tunes

--- Eutermea.hs
+++ Eutermea.hs
@@ -6,3 +6,6 @@ import Control.DeepSeq
 -- port number is installation specific
 play :: (ToMusic1 a, NFData a) => Music a -> IO ()
 play = playDev 2
+
+fromList :: [Music Pitch] -> Music Pitch
+fromList = foldl (:+:) (rest 0)
