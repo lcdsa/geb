9ba15b364374c05c7e07df831aa470b46fafe20f
Arianna Masciolini <uzkamascio@gmail.com>
2021-01-17 19:19:28 +0100

    added comments and canon function

--- Eutermea.hs
+++ Eutermea.hs
@@ -3,9 +3,14 @@ module Eutermea where
 import Euterpea
 import Control.DeepSeq
 
--- port number is installation specific
+-- | Plays music on systems where the default port is != 0
+-- (port number is installation specific)
 play :: (ToMusic1 a, NFData a) => Music a -> IO ()
 play = playDev 2
 
 fromList :: [Music Pitch] -> Music Pitch
 fromList = foldl (:+:) (rest 0)
+
+-- | Given the duration of a rest and tune, it returns a canon of that tune
+canon :: Dur -> Music a -> Music a
+canon offset tune = tune :=: (rest offset :+: tune)
