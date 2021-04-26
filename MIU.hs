96bb6847860ffebbbee2d0f9bd30c88a0e37d3d7
kappanneo <pannacciclaudio@gmail.com>
2021-04-26 08:09:57 +0200

    MIU derivations and more readable pag

--- MIU.hs
+++ MIU.hs
@@ -1,9 +1,8 @@
 module MIU where
 
-import Prelude hiding ( Char, String )
 import Parsing ( oneOrMore, parse, readsP )
 import Data.List ( isInfixOf )
-import Data.Maybe ( maybeToList )
+import Data.Maybe ( maybeToList, fromJust )
 import Test.QuickCheck ( elements, Arbitrary(arbitrary) )
 
 -- pag 36 --
@@ -33,39 +32,61 @@ instance Arbitrary MIU.Char where
 
 -- pag 37 --
 
-theorems :: [MIU.String]
-theorems = [read "MI" :: MIU.String]
-
 type Rule = Int
 
-rule :: Rule -> MIU.String -> MIU.String
-rule 1 miu | last miu == I = miu ++ [U]
-rule 2 (m:iu) = m:iu ++ iu
-rule 3 miu | iii `isInfixOf` miu = rule3 miu
+rule :: Rule -> MIU.String -> Maybe MIU.String
+rule 1 miu | last miu == I       = Just $ miu ++ [U]
+rule 2 (m:iu)                    = Just $ m:iu ++ iu
+rule 3 miu | iii `isInfixOf` miu = Just $ rule3 miu
  where
    iii = replicate 3 I
    rule3 :: MIU.String -> MIU.String
    rule3 (I:I:I:iu) = U:iu
    rule3 (m:iu) = m : rule3 iu
    rule3 [] = []
-rule 4 miu | uu `isInfixOf` miu = rule4 miu
+rule 4 miu | uu `isInfixOf` miu  = Just $ rule4 miu
  where
    uu = replicate 2 U
    rule4 :: MIU.String -> MIU.String
    rule4 (U:U:iu) = iu
    rule4 (m:iu) = m : rule4 iu
    rule4 [] = []
-rule _ miu = miu
+rule _ _ = Nothing
 
--- pag 39 --
+-- pag 38 --
 
-derivation :: [MIU.String]
-derivation
- = do let m1 = read "MI"
-      let m2 = rule 2 m1
-      let m3 = rule 2 m2
-      let m4 = rule 1 m3
-      let m5 = rule 3 m4
-      let m6 = rule 2 m5
-      let m7 = rule 4 m6
-      [m1,m2,m3,m4,m5,m6,m7]
+type Index = Int
+type Theorem = MIU.String
+newtype Derivation = Derivation [(Index, Rule)]
+
+axioms :: [Theorem]
+axioms = [read "MI"] -- MI is the only axiom
+
+theorems :: Derivation -> Maybe [Theorem]
+theorems = theorems' axioms
+
+theorems' :: [Theorem] -> Derivation -> Maybe [Theorem]
+theorems' ts (Derivation []) = Just ts
+theorems' ts (Derivation ((i,r):d)) = do -- Maybe
+   t <- rule r (ts !! (i - 1))
+   theorems' (ts ++ [t]) (Derivation d)
+
+sequential :: [Rule] -> Derivation
+sequential = Derivation . zip [1..]
+
+instance Show Derivation where
+   show d@(Derivation xs) = unlines $ zipWith3
+      (\ i t s -> "(" ++ show i ++ ")" ++ "  " ++ show t ++ "\t\t" ++ s)
+      [1..]
+      (fromJust (theorems d))
+      ( replicate (length axioms) ('\t':"assioma")
+         ++ map 
+            ( \(i,r) -> "da (" ++ show i ++ ") con la regola " ++ roman r )
+            xs
+      )
+    where 
+      roman :: Rule -> Prelude.String
+      roman 1 = "I"
+      roman 2 = "II"
+      roman 3 = "III"
+      roman 4 = "IV"
--- Pagine.hs
+++ Pagine.hs
@@ -15,42 +15,95 @@ import Test.QuickCheck
 
 pag :: Int -> IO ()
 pag 6 = do
-      putStrLn "Il Tema Regio"
-      play royalTheme 
+   putStrLn "Il Tema Regio"
+   play royalTheme 
 pag 9 = do
-      putStrLn "Canone inverso di Scott E. Kim sul tema Good King Wenceslas"
-      play $ theme :=: variation
-      putStrLn "Canone inverso \"algoritmico\" sul tema Good King Wenceslas"
-      play $ inverseCanon (2/4) theme
+   putStrLn "Canone inverso di Scott E. Kim sul tema Good King Wenceslas"
+   play $ theme :=: variation
+   putStrLn "Canone inverso \"algoritmico\" sul tema Good King Wenceslas"
+   play $ inverseCanon (2/4) theme
 pag 36 = do
    putStrLn "Alcune stringhe del sistema MIU:"
    sample (arbitrary :: Gen MIU.String)
    putStrLn ""
-pag 37 = do
-   putStrLn "Stringhe in nostro possesso:"
-   putStrLn ""
-   putStrLn $ unlines $ map show theorems
-pag 80 = do
-      putStrLn $ "Ecco un rompicapo su cui riflettere (...): " 
-            ++ "come si può caratterizzare il seguente insieme di numeri "
-            ++ "interi (o il suo spazio negativo)?"
-      putStrLn ""
-      print figure
-      putStrLn ""
-      putStrLn "Qualche indizio/osservazione:"
-      putStrLn $ "- lo \"sfondo\" (o complemento) di questa \"figura\": " 
-      print $ background figure
-      putStrLn $ "- il numero successivo: " ++ show (fromJust $ next figure)
-      putStrLn "- la sequenza \"allungata\" di 5: "
-      print $ continue' figure 5
-pag 397 = do 
-      putStrLn "IL MASCELLONTE"
-      putStrLn $ "traduzione italiana alternativa del \"Jabberwocky\""
-              ++ "di Lewis Carrol, a cura di Arianna Masciolini"
-      putStrLn ""
-      putStrLn "-----------------------------------------"
-      putStrLn ""
-      putStrLn jabberwocky
+pag 37 = putStr $ unlines
+   [ ""
+   , "Stringhe in nostro possesso:"
+   , ""
+   , unlines $ map show axioms
+
+   , "Regole di derivazione:"
+   , ""
+   , "Regola I: Se si possiede una stringa che termina con una I,"
+      ++ " si può aggiungere una U alla fine."
+   , ""
+   , "Regola II: Si abbia Mx."
+      ++ " Allora si può includere Mxx nella collezione."
+   , ""
+   , "Esempi:"
+   , unlines $ map 
+      ( \s -> "Da " ++ s ++ " si può ottenere "
+               ++ (show . fromJust) (rule 2 (read s))
+               ++ "."
+      ) 
+      ["MIU", "MUM", "MU"]
+   , "Regola III: Se in una delle stringhe della collezione c'è III,"
+      ++ " si può costruire una nuova stringa sostituendo U al posto di III."
+   , ""
+   , "Esempi:"
+   , unlines $ map
+      ( \s -> let s' = rule 3 (read s) in
+         "A partire da " ++ s 
+         ++ (if isNothing s' then " non"   else "" ) ++ " si può costruire "
+         ++ (if isNothing s' then "niente" else show $ fromJust s')
+         ++ "."
+      ) 
+      ["UMIIIMU","MIIII","IIMII","MIII"]
+   ]
+pag 38 = putStr $ unlines
+   [ ""
+   , "Regola IV: Se all'interno di una delle stringhe della collezione c'è UU,"
+      ++ " si può eliminarlo."
+   , ""
+   , "Esempi:"
+   , unlines $ map 
+      ( \s -> "Da " ++ s ++ " si ottiene "
+               ++ (show . fromJust) (rule 4 (read s))
+               ++ "."
+      ) 
+      ["UUU", "MUUUIII"]   
+   ]
+pag 39 = putStr $ unlines
+   [ ""
+   , "Ecco una derivazione del teorema MUIIU:"
+   , show (sequential [2,2,1,3,2,4])
+   ]
+pag 80 = putStr $ unlines
+   [ ""
+   , "Ecco un rompicapo su cui riflettere (...): " 
+   , "come si può caratterizzare il seguente insieme di numeri "
+      ++ "interi (o il suo spazio negativo)?"
+   , ""
+   , show figure
+   , ""
+   , "Qualche indizio/osservazione:"
+   , ""
+   , "- lo \"sfondo\" (o complemento) di questa \"figura\": " 
+   , show (background figure)
+   , "- il numero successivo: " ++ show (fromJust $ next figure)
+   , "- la sequenza \"allungata\" di 5: "
+   , show (continue' figure 5)
+   ]
+pag 397 = putStr $ unlines
+   [ ""
+   , "IL MASCELLONTE"
+   , "traduzione italiana alternativa del \"Jabberwocky\""
+      ++ "di Lewis Carrol, a cura di Arianna Masciolini"
+   , ""
+   , "-----------------------------------------"
+   , ""
+   , jabberwocky
+   ]
 pag _ = putStrLn "Niente di interessante..."
 
 main = undefined
